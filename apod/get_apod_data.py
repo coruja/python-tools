731c9085d6e6c8a865ca336f335ec3716527c294
coruja <coruja074@gmail.com>
2013-05-19 11:02:35 +0200

    remove stuff from comments

--- apod/get_apod_data.py
+++ apod/get_apod_data.py
@@ -18,6 +18,7 @@ APOD_HTML = '/astropix.html'
 BLANK_SPACES = re.compile('\s+')
 HTML_TAGS_RE = r'</?\S([^=>]*=(\s*"[^"]*"|\s*\'[^\']*\'|\S*)|[^>])*?>'
 STRIP_TAGS = re.compile(HTML_TAGS_RE, re.IGNORECASE)
+SKIP_CAPTION = False
 
 def split_lines(text):
     """ Split lines longer than 80 characters """
@@ -37,7 +38,29 @@ def _get_url(url):
     soup = BeautifulSoup(doc)
     res = [x['href'] for x in soup.findAll('a', href=True) \
                         if IMAGE_DIR in x['href']]
-    return page_as_ls, res
+    title = soup.title.string.strip()
+    return title, page_as_ls, res
+
+TO_IGNORE = ("Tomorrow's picture", 'Videos & Discussion', 'Ask Me Anything',
+"Best of APOD", "Follow APOD", "APOD Public Talk",
+"Occasionally humorous", "APOD editor",
+"Did a relative see this", "Facebook", "Twitter")
+
+def _get_explanation(page_as_ls):
+    explanation = []
+    found = False
+    for i in page_as_ls:
+        if 'Explanation:' in i:
+            found = True
+            continue
+        if found:
+            for l in TO_IGNORE:
+                if l.lower() in i.lower():
+                    return explanation
+        if found:
+            sentence = re.sub(BLANK_SPACES, ' ', i.replace(os.linesep,' '))
+            explanation.append(sentence)
+    return explanation
 
 def main(args):
     """ The main function """
@@ -47,25 +70,16 @@ def main(args):
         url = APOD_BASE_URL + APOD_HTML
 
     # Fech the data from the given APOD url
-    page_as_ls, res = _get_url(url)
+    title, page_as_ls, res = _get_url(url)
     if not res:
         sys.exit("Could not find %s at %s" % (IMAGE_DIR, url))
     img_file_url = APOD_BASE_URL + '/' + res[0]
     print img_file_url
 
-    explanation = []
-    found = False
-    for i in page_as_ls:
-        if 'Explanation:' in i:
-            found = True
-            continue
-        if found and 'Videos & Discussion' in i:
-            break
-        if found and "Tomorrow's picture:" in i:
-            break
-        if found:
-            sentence = re.sub(BLANK_SPACES, ' ', i.replace(os.linesep,' '))
-            explanation.append(sentence)
+    if SKIP_CAPTION:
+        return 0
+
+    explanation = _get_explanation(page_as_ls)
 
     # Trick to also detect jpg and JPG case insensitive
     case_insensitive_re = re.compile(re.escape('jpg'), re.IGNORECASE)
@@ -74,7 +88,8 @@ def main(args):
     html = ''.join(explanation)    
     text = STRIP_TAGS.sub('', html).lstrip()
     explanation_f = open(explanation_fn, 'w')
-    explanation_f.write(split_lines(text))
+    contents = title + ":\n" + split_lines(text)
+    explanation_f.write(contents)
     explanation_f.close()
 
 if __name__ == "__main__":
