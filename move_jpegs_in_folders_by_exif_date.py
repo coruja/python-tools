c86cf36ebcf2811b2921742b8dd8ee7825dd83cf
coruja <coruja074@gmail.com>
2013-07-20 15:51:22 +0200

    added option move

--- move_jpegs_in_folders_by_exif_date.py
+++ move_jpegs_in_folders_by_exif_date.py
@@ -30,6 +30,7 @@ def main():
     parser = OptionParser(usage='Usage: %prog [OPTIONS] PATH')
     parser.add_option('--dry-run', action='store_true', default=False, dest='dry_run', help='display the actions that will be undertaken without actually executing them')
     parser.add_option('--recursive', action='store_true', default=False, dest='recursive', help='traverse subdirectories recursively')
+    parser.add_option('--move', action='store_true', default=False, dest='move', help='move the files instead of doing a copy')
     (options, args) = parser.parse_args()
     # Use the current directory if none has been specified
     if len(args) != 1:
@@ -42,9 +43,9 @@ def main():
     directory = os.path.realpath(os.path.normcase(directory))
     # Get the current working dir
     cwdir = os.getcwdu()
-    run(cwdir, directory, options.dry_run, options.recursive)
+    run(cwdir, directory, options.dry_run, options.recursive, options.move)
 
-def do_copy(src, dest, dry_run=False):
+def do_copy(src, dest, dry_run=False, domove=False):
     # Move the photo into the directory
     do_copy = True
     source_ck = md5_incomplete_ck(src)
@@ -56,14 +57,17 @@ def do_copy(src, dest, dry_run=False):
             do_copy = False
     if do_copy and not dry_run:
         print moving_msg,
-        shutil.copy2(src, dest)
+        if not domove:
+            shutil.copy(src, dest)
+        else:
+            shutil.move(src,dest)
     elif not do_copy and not dry_run:
         print moving_msg,
     elif dry_run:
         print "[dry-run]", moving_msg,
     print "Done."
 
-def run(cwdir, directory, dry_run, recursive):
+def run(cwdir, directory, dry_run, recursive, domove):
     files = []
     if not recursive:
         files = find_case_insensitve(os.path.join(directory, '*'), EXTENSIONS)
@@ -80,28 +84,32 @@ def run(cwdir, directory, dry_run, recursive):
         metadata = pyexiv2.ImageMetadata(f)
         metadata.read()
 
+        if not metadata:
+            print "No EXIF metadata found in %s" % f
+            continue
+
         for i in metadata.exif_keys:
             if EXIF_DATE_KEY in i:
                 break
         else:
+            print EXIF_DATE_KEY, "not found in %s" % f
             continue
 
         file_name = os.path.basename(f)
         date = metadata[EXIF_DATE_KEY].value
-        #date_directory_name = "%d_%02d_%02d" % (date.year, date.month, date.day)
-        date_directory_name = "%d_%02d" % (date.year, date.month)
+        date_directory_name = os.path.join(str(date.year), "%02d" % (date.month))
         # Create the directory
         date_directory_path = os.path.join(cwdir, date_directory_name)
         if not os.path.exists(date_directory_path):
             directory_msg = "Creating directory '%s'..." % date_directory_path
             if not dry_run:
                 print directory_msg,
-                os.mkdir(date_directory_path)
+                os.makedirs(date_directory_path)
             else:
                 print '[dry-run]', directory_msg,
             print 'Done.'
         moved_file_path = os.path.join(date_directory_path, file_name)
-        do_copy(f, moved_file_path, dry_run)
+        do_copy(f, moved_file_path, dry_run, domove)
 
 if __name__ == "__main__":
     main()
